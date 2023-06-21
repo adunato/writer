4753f9159a01966bf111464d11fa8109f11b1120
Daniele <daniele.torrigiani@gmail.com>
2023-06-21 16:36:11 +0100

    Bug fix when collation is disabled

--- writer_utils.py
+++ writer_utils.py
@@ -4,7 +4,7 @@ from .writer_params import input_elements
 
 def copycontent(enabled, new_input, existing_text, chapter_separator):
     if(enabled == False):
-        return
+        return ""
     if(chapter_separator != ""):
         return existing_text+new_input+chapter_separator
     else:
